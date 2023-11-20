43d9b6ecd2234f032b1d02fd070c952777219293
Jhon Carlos Acevedo Mendoza <jhoncarlosacevedomendoza@gmail.com>
2023-11-20 15:11:08 -0500

    add my intro on the header

---
+++ src/data/work.json.ts
@@ -0,0 +1,113 @@
+export interface Template {
+  link: string;
+  title: string;
+
+  img: string;
+};
+const one: Template = {
+  link: "#",
+  img: "/1.png",
+  title: "Lexington Themes",
+};
+const two = {
+  link: "#",
+  img: "/2.png",
+  title: "Creative Solutions",
+};
+
+const three = {
+  link: "#",
+  img: "/3.png",
+  title: "Digital Innovations",
+};
+
+const four = {
+  link: "#",
+  img: "/4.png",
+  title: "WebCrafters",
+};
+
+const five = {
+  link: "#",
+  img: "/5.png",
+  title: "PixelPerfect Designs",
+};
+
+const six = {
+  link: "#",
+  img: "/6.png",
+  title: "CodeMasters",
+};
+
+const seven = {
+  link: "#",
+  img: "/7.png",
+  title: "Web Wizards",
+};
+
+const eight = {
+  link: "#",
+  img: "/8.png",
+  title: "TechCreators",
+};
+
+const nine = {
+  link: "#",
+  img: "/9.png",
+  title: "WebArtistry",
+};
+
+const ten = {
+  link: "#",
+  img: "/10.png",
+  title: "DigitalCraftsmen",
+};
+const eleven = {
+  link: "#",
+  img: "/11.png",
+  title: "SiteSculptors",
+};
+const twelve = {
+  link: "#",
+  img: "/12.png",
+  title: "WebWorks Pro",
+};
+
+const thirteen = {
+  link: "#",
+  img: "/13.png",
+  title: "CodeArtisans",
+};
+
+const fourteen = {
+  link: "#",
+  img: "/14.png",
+  title: "TechFusion",
+};
+
+const fifteen = {
+  link: "#",
+  img: "/15.png",
+  title: "DigitalCrafters",
+};
+
+
+export const bytitle = {
+  one,
+    two,
+    three,
+  four,
+five,
+six,
+seven,
+eight,
+  nine,
+  ten,
+  eleven,
+  thirteen,
+  twelve,
+  fourteen,
+  fifteen,
+
+};
+export const work = Object.values(bytitle);
